92bdb6f934c3d54e477e0f2ffe85ff4a3d75be2f
OnYou Kang <onyou.kang@wustl.edu>
2019-04-06 15:49:04 -0500

    push

---
+++ js/javascript.js
@@ -0,0 +1,62 @@
+//page transition
+$(document).ready(function() {
+  setTimeout(function(){ 
+    $('body').animate({opacity:1.0},2000); 
+  }, 400);
+
+  $('a').click(function(event) {
+    event.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut(1500, newpage);
+  });
+
+  function newpage() {
+    window.location = newLocation;
+  }
+
+  window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
+});
+
+//search bar
+$(document).ready(function(){
+  $.ajaxSetup({ cache: false });
+  $('#search').keyup(function(){
+    $('#result').html('');
+    $('#state').val('');
+    var searchField = $('#search').val();
+    var expression = new RegExp(searchField, "i");
+    $.getJSON('data.json', function(data) {
+      $.each(data, function(key, value){
+        if (value.name.search(expression) != -1) {
+          $('#result').removeClass('active');
+          $('#result').append('<li class="list-group-item link-class"><a href="'+value.image+'" height="40" width="40" class="img-thumbnail">'+value.name+'</a>');
+        }
+      });   
+    });
+  });
+
+  hide = true;
+  $('body').on("click", function () {
+    if (hide) $('#result').addClass('active');
+    hide = true;
+  });
+
+  $('#result').on('click', 'li', function() {
+    var click_text = $(this).text().split('|');
+    $('#search').val($.trim(click_text[0]));
+    $("#result").html('');
+  });
+});
+
+//audio play
+$(document).ready(function() {
+    $("#my_audio").get(0).play();
+});
